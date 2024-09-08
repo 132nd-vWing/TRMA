dd28793780dd8d893f1b463f16eb8b67b5688eff
132nd.Neck <neck.dcs@gmail.com>
2024-09-08 17:56:21 +0200

    chg: Adjusted F16 bort and datalink from 3xx to 4xx (494th uses 3xx series). Adjusted R23 IADS for testing purposes.

---
+++ l10n/DEFAULT/skynet-iads-setup-TRMA_R23_IADS.lua
@@ -0,0 +1,25 @@
+do
+--create an instance of the IADS
+redIADS = SkynetIADS:create('R23_IADS')
+
+
+--add all units with unit name beginning with 'EWR' to the IADS:
+redIADS:addEarlyWarningRadarsByPrefix('R23_EWR')
+
+
+--add all groups begining with group name 'IADS' to the IADS:
+redIADS:addSAMSitesByPrefix('R23_IADS')
+
+-- Point Defence for SA-2
+local sa15 = redIADS:getSAMSiteByGroupName('R23_IADS_SA15_pointdefence_1')
+redIADS:getSAMSiteByGroupName('R23_IADS_SA2-1'):addPointDefence(sa15):setHARMDetectionChance(100)
+
+local sa15 = redIADS:getSAMSiteByGroupName('R23_IADS_SA15_pointdefence_2')
+redIADS:getSAMSiteByGroupName('R23_IADS_SA2-1'):addPointDefence(sa15):setHARMDetectionChance(100)
+
+
+
+--activate the IADS
+redIADS:activate()
+
+end
