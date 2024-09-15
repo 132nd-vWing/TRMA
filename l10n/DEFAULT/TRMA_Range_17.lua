fb591896353db20d42e4faf9bde52fdd744568b4
132nd.Neck <neck.dcs@gmail.com>
2024-09-15 13:34:46 +0200

    chg: Added Range 17 F10 menu option to start AR Scenario

--- l10n/DEFAULT/TRMA_Range_17.lua
+++ l10n/DEFAULT/TRMA_Range_17.lua
@@ -1,9 +1,16 @@
 range_17_menu_root = MENU_MISSION:New("Range 17",range_root_menu13_18)
 
 local function range17_flag101()
-  range_17_menu_ARscenario_Armored_Brigade:Remove()
+  range_17_menu_ARscenario_ArmoredBrigade:Remove()
   trigger.action.setUserFlag(101, true)
   MessageToAll("R17 AR scenario Armored Brigade activated")
 end
 
-range_17_menu_ARscenario_Armored_Brigade = MENU_MISSION_COMMAND:New("Activate AR scenario Armored Brigade",range_17_menu_root,range17_flag101)
+range_17_menu_ARscenario_ArmoredBrigade = MENU_MISSION_COMMAND:New("Activate AR scenario Armored Brigade",range_17_menu_root,range17_flag101)
+
+
+
+
+
+
+
