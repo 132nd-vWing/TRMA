0140ec6c043416b23a963ad3bd435b10ac4a64dc
132ndNeck <neck.dcs@gmail.com>
2025-11-03 18:27:06 +0100

    chg: Added SAMs for R24 AR script

--- l10n/DEFAULT/TRMA_R24_AR_Armored_Brigade.lua
+++ l10n/DEFAULT/TRMA_R24_AR_Armored_Brigade.lua
@@ -1,4 +1,5 @@
--- Lua script to activate 1 of 3 primary groups, 6 of 12 additional groups, and possibly 1 of 3 optional groups with a 50% chance of no activation
+-- Lua script to activate 1 of 3 primary groups, 6 of 12 additional groups, possibly 1 of 3 optional groups, 
+-- and 1 of 6 SAM groups.
 
 -- List of the first 3 primary groups
 local primaryGroups = {
@@ -18,76 +19,57 @@ local optionalGroups = {
     "R24_Surface_surface_BN", "R24_Rocket_artillery_BN", "R24_Heavy_Rocket_artillery_BN"
 }
 
+-- List of 6 SAM random groups
+local samGroups = {
+    "R24_SAM_Random1", "R24_SAM_Random2", "R24_SAM_Random3",
+    "R24_SAM_Random4", "R24_SAM_Random5", "R24_SAM_Random6"
+}
+
 -- Function to activate a random group from the first 3
 function activateRandomPrimaryGroup()
-    -- Select a random index between 1 and the number of primary groups
     local randomIndex = math.random(1, #primaryGroups)
-    
-    -- Get the group name for the selected index
     local selectedGroup = primaryGroups[randomIndex]
-    
-    -- Activate the group
     trigger.action.activateGroup(Group.getByName(selectedGroup))
-
-    -- Print a message to confirm which group was activated (optional, for debug)
     --trigger.action.outText("Activating primary group: " .. selectedGroup, 10)
 end
 
 -- Function to activate 6 random groups from the additional 12
 function activateRandomAdditionalGroups()
-    -- Create a copy of the additional groups table to work with
     local groupsPool = {unpack(additionalGroups)}
-    
-    -- Table to store the 6 selected groups
     local selectedGroups = {}
-    
-    -- Select 6 unique groups
     for i = 1, 6 do
-        -- Select a random index from the remaining groups
         local randomIndex = math.random(1, #groupsPool)
-        
-        -- Get the group name for the selected index
         local selectedGroup = groupsPool[randomIndex]
-        
-        -- Add selected group to the table
         table.insert(selectedGroups, selectedGroup)
-        
-        -- Remove the selected group from the pool to prevent duplicates
         table.remove(groupsPool, randomIndex)
     end
-    
-    -- Activate the selected groups
     for _, groupName in ipairs(selectedGroups) do
         trigger.action.activateGroup(Group.getByName(groupName))
-        -- Print a message to confirm which groups were activated (optional, for debug)
-       -- trigger.action.outText("Activating additional group: " .. groupName, 10)
+        --trigger.action.outText("Activating additional group: " .. groupName, 10)
     end
 end
 
 -- Function to optionally activate one group from optional groups, or none
 function activateOptionalGroup()
-    -- 50% chance to skip activation
     local shouldActivate = math.random() > 0.5
-    
     if shouldActivate then
-        -- Select a random group from the optional list
         local randomIndex = math.random(1, #optionalGroups)
-        
-        -- Get the group name for the selected index
         local selectedGroup = optionalGroups[randomIndex]
-        
-        -- Activate the group
         trigger.action.activateGroup(Group.getByName(selectedGroup))
-        
-        -- Print a message to confirm which optional group was activated (optional, for debug)
-      --  trigger.action.outText("Activating optional group: " .. selectedGroup, 10)
-    else
-        -- Print a message to confirm no optional group was activated (optional, for debug)
-        --trigger.action.outText("No optional group activated", 10)
+        --trigger.action.outText("Activating optional group: " .. selectedGroup, 10)
     end
+end
+
+-- Function to activate one random SAM group
+function activateRandomSAMGroup()
+    local randomIndex = math.random(1, #samGroups)
+    local selectedGroup = samGroups[randomIndex]
+    trigger.action.activateGroup(Group.getByName(selectedGroup))
+    --trigger.action.outText("Activating SAM group: " .. selectedGroup, 10)
 end
 
 -- Call the functions to activate the groups
 activateRandomPrimaryGroup()          -- Activates 1 of the 3 primary groups
 activateRandomAdditionalGroups()      -- Activates 6 of the 12 additional groups
 activateOptionalGroup()               -- 50% chance to activate 1 of 3 optional groups or none
+activateRandomSAMGroup()              -- Activates 1 of 6 SAM groups
