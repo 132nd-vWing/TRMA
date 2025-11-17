26ccf44bd7c3ec14e5fe65b1aeea5a82ed7e5376
jonde99 <jonde99@yahoo.com>
2025-11-17 15:50:36 +0000

    CAS Scenario menus added

--- l10n/DEFAULT/TRMA_Range_15.lua
+++ l10n/DEFAULT/TRMA_Range_15.lua
@@ -22,11 +22,6 @@ end
 range_15_scenario["range_15_menu_flag61"] = MENU_MISSION_COMMAND:New("Activate R15 Basic CAS Scenario",range_15_menu_root,range15_flag61)
 range_15_scenario["range_15_menu_flag35"] = MENU_MISSION_COMMAND:New("Activate R15 CAS Scenario-1",range_15_menu_root,range15_flag35)
 
-
-
-
-range_15_menu_root = MENU_MISSION:New("Range 15",range_root_menu13_18)
-
 local function range15_flag65()
   range_15_menu_AR:Remove()
   trigger.action.setUserFlag(65, true)
