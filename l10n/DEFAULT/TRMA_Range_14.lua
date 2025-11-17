2dd9dc1e8d29442a374f884669cb6a78a14292e5
jonde99 <jonde99@yahoo.com>
2025-11-17 16:24:36 +0000

    Menu items fixed

--- l10n/DEFAULT/TRMA_Range_14.lua
+++ l10n/DEFAULT/TRMA_Range_14.lua
@@ -1,24 +1,25 @@
 range_14_menu_root = MENU_MISSION:New("Range 14",range_root_menu13_18)
-range_14_scenario = {}
 
-local function range_14_clear_menu()
-    for k, v in pairs(range_14_scenario) do
-        v:Remove()
-        range_14_scenario[k] = nil
-    end
-end
+--range_14_scenario = {}
+
+--local function range_14_clear_menu()
+--    for k, v in pairs(range_14_scenario) do
+--        v:Remove()
+--        range_14_scenario[k] = nil
+--    end
+--end
 
 local function range14_flag31()
-    range_14_clear_menu()
+    range_14_menu_flag31:Remove()
     trigger.action.setUserFlag(31, true)
     MessageToAll("R14 Rognan scenario activated")
 end
 
 local function range14_flag34()
-    range_14_clear_menu()
+    range_14_menu_flag34:Remove()
     trigger.action.setUserFlag(34, true)
-    MessageToAll("R14 Cas Scenario-1 activated")
+    MessageToAll("R14 CAS scenario #1 activated")
 end
 
-range_14_scenario["range_14_menu_flag31"] = MENU_MISSION_COMMAND:New("Activate R14 Rognan Scenario",range_14_menu_root,range14_flag31)
-range_14_scenario["range_14_menu_flag34"] = MENU_MISSION_COMMAND:New("Activate R14 CAS Scenario-1",range_14_menu_root,range14_flag34)
+range_14_menu_flag31 = MENU_MISSION_COMMAND:New("Activate R14 Rognan Scenario",range_14_menu_root,range14_flag31)
+range_14_menu_flag34 = MENU_MISSION_COMMAND:New("Activate R14 CAS Scenario #1",range_14_menu_root,range14_flag34)
--- l10n/DEFAULT/TRMA_Range_15.lua
+++ l10n/DEFAULT/TRMA_Range_15.lua
@@ -1,31 +1,31 @@
 range_15_menu_root = MENU_MISSION:New("Range 15",range_root_menu13_18)
-range_15_scenario = {}
 
-local function range_15_clear_menu()
-  for k, v in pairs(range_15_scenario) do
-    v:Remove()
-    range_15_scenario[k] = nil
-  end
-end
+--range_15_scenario = {}
+
+--local function range_15_clear_menu()
+--  for k, v in pairs(range_15_scenario) do
+--    v:Remove()
+--    range_15_scenario[k] = nil
+--  end
+--end
 
 local function range15_flag61()
-  range_15_clear_menu()
+  range_15_menu_flag61:Remove()
   trigger.action.setUserFlag(61, true)
   MessageToAll("R15 Basic CAS scenario activated")
 end
 local function range15_flag35()
-  range_15_clear_menu()
+  range_15_menu_flag35:Remove()
   trigger.action.setUserFlag(35, true)
-  MessageToAll("R15 CAS scenario-1 activated")
+  MessageToAll("R15 CAS scenario #2 activated")
 end
 
-range_15_scenario["range_15_menu_flag61"] = MENU_MISSION_COMMAND:New("Activate R15 Basic CAS Scenario",range_15_menu_root,range15_flag61)
-range_15_scenario["range_15_menu_flag35"] = MENU_MISSION_COMMAND:New("Activate R15 CAS Scenario-1",range_15_menu_root,range15_flag35)
-
 local function range15_flag65()
-  range_15_menu_AR:Remove()
+  range_15_menu_flag65:Remove()
   trigger.action.setUserFlag(65, true)
   MessageToAll("Range 15 AR scenario activated")
 end
 
-range_15_menu_AR = MENU_MISSION_COMMAND:New("Activate Range 15 AR Scenario",range_15_menu_root,range15_flag65)
+range_15_menu_flag61 = MENU_MISSION_COMMAND:New("Activate R15 Basic CAS Scenario",range_15_menu_root,range15_flag61)
+range_15_menu_flag35 = MENU_MISSION_COMMAND:New("Activate R15 CAS Scenario #2",range_15_menu_root,range15_flag35)
+range_15_menu_flag65 = MENU_MISSION_COMMAND:New("Activate Range 15 AR Scenario",range_15_menu_root,range15_flag65)
