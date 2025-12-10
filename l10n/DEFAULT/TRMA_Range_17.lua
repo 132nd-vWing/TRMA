71a60c4222bc8c7744f6eb554c501fefebc1c970
jonde99 <jonde99@yahoo.com>
2025-12-10 08:50:43 +0000

    494MQ added to R17.

--- l10n/DEFAULT/TRMA_Range_17.lua
+++ l10n/DEFAULT/TRMA_Range_17.lua
@@ -11,8 +11,16 @@ local function range17_flag102()
   trigger.action.setUserFlag(102, true)
   MessageToAll("R17 Air Defenses at Strike Target activated")
 end
+
+local function range17_flag_494MQT()
+  range_17_menu_494MQT:Remove()
+  trigger.action.setUserFlag("494MQT", true)
+  MessageToAll("R17 494 MQT activated")
+end
+
 range_17_menu_ARscenario_ArmoredBrigade = MENU_MISSION_COMMAND:New("Activate AR scenario Armored Brigade",range_17_menu_root,range17_flag101)
 range_17_menu_StrikeDefenses = MENU_MISSION_COMMAND:New("Activate Airdefenses at Range 17 Strike Targets",range_17_menu_root,range17_flag102)
+range_17_menu_494MQT = MENU_MISSION_COMMAND:New("Activate 494th MQT Scenario",range_17_menu_root,range17_flag_494MQT)
 
 
 
